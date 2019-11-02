3ed309a2d4c2012ae9b460414443f23db752efc5
shagbag913 <sh4gbag913@gmail.com>
2019-11-02 10:37:52 -0400

    Add GahsROM device configurations

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,11 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
-	$(LOCAL_DIR)/aosp_sailfish.mk
+	$(LOCAL_DIR)/aosp_sailfish.mk \
+	$(LOCAL_DIR)/gahs_marlin.mk \
+	$(LOCAL_DIR)/gahs_sailfish.mk
 
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
-	aosp_sailfish-userdebug
+	aosp_sailfish-userdebug \
+	gahs_marlin-userdebug \
+	gahs_sailfish-userdebug
---
+++ gahs_sailfish.mk
@@ -0,0 +1,23 @@
+# Inherit common device config
+$(call inherit-product, device/google/marlin/aosp_sailfish.mk)
+
+# Inherit proprietary blobs
+$(call inherit-product, vendor/google/sailfish/sailfish-vendor.mk)
+
+# Inherit Gahs vendor configuration
+$(call inherit-product, vendor/gahs/config/common.mk)
+
+PRODUCT_NAME := gahs_sailfish
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Override some prop values in order to pass SafetyNet
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sailfish \
+    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
+
+BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
