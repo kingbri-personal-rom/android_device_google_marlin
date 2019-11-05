82ac30cfb2fd911cc708ca8c559ded0c417bf546
shagbag913 <sh4gbag913@gmail.com>
2019-11-04 20:52:51 -0500

    gahs_{marlin,sailfish}: remove SafetyNet workarounds
     * No longer works, not willing to go through decompilation hell to
       figure it out

--- gahs_sailfish.mk
+++ gahs_sailfish.mk
@@ -11,13 +11,3 @@ PRODUCT_NAME := gahs_sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 PRODUCT_RESTRICT_VENDOR_FILES := false
-
-# Override some prop values in order to pass SafetyNet
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
-
-BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
