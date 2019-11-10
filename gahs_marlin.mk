88bbc72fe982f10c2f5a188b158cccbc9935cb5d
kingbri <bdashore3@gmail.com>
2019-11-09 20:22:51 -0500

    Revert "gahs_{marlin,sailfish}: remove SafetyNet workarounds"
    This reverts commit 82ac30cfb2fd911cc708ca8c559ded0c417bf546.

--- gahs_marlin.mk
+++ gahs_marlin.mk
@@ -11,3 +11,13 @@ PRODUCT_NAME := gahs_marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Override some prop values in order to pass SafetyNet
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A1 5908163 release-keys"
+
+BUILD_FINGERPRINT := google/marlin/marlin:10/QP1A.191005.007.A1/5908163:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
